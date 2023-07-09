9d5e2b25a6e25ef3a386199d888a98181068d131
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-07-09 15:39:03 +0200

    copy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
+import React from 'react';
+// import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { useEffect } from 'react';
+import { selectIsLoading,selectError } from './redux/selectors';
+import { useDispatch } from 'react-redux';
+
+import { useSelector } from 'react-redux';
+import { fetchContacts } from './redux/operations';
+
 export const App = () => {
+  
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError)
+
+  const dispatch = useDispatch();
+
+  useEffect(()=> {dispatch(fetchContacts());}, [dispatch])
+
+
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+
+      <h2> Contacts </h2>
+      <Filter />
+      {isLoading && !error && <b>Request in progress.....</b>}
+      <ContactList
+      />
     </div>
   );
 };
